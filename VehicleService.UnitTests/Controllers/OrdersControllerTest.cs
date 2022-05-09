97d38a3f926a8522cc69f8733fa83fea77e486b0
ArtRodriguez <87017184+ArtRodriguez@users.noreply.github.com>
2022-05-09 15:30:48 +0200

    Controller tests

--- VehicleService.UnitTests/Controllers/OrdersControllerTest.cs
+++ VehicleService.UnitTests/Controllers/OrdersControllerTest.cs
@@ -1,12 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Threading;
 using System.Threading.Tasks;
+using VehicleService.API.Application.Commands.InsertOrder;
+using VehicleService.API.Controllers;
+using VehicleService.Infrastructure.Queries.Orders;
+using Xunit;
 
 namespace VehicleService.UnitTests.Controllers
 {
     public class OrdersControllerTest
     {
+        [Fact]
+        public async Task Post_Handler_Returns_True_Ok()
+        {
+            // Arrange
+            var mediatorStub = new Mock<IMediator>();
+            var loggerStub = new Mock<ILogger<OrdersController>>();
+            var orderQueriesStub = new Mock<IOrderQueries>();
+            mediatorStub
+                .Setup(x => x.Send(It.IsAny<IRequest<bool>>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(true));
+            var ordersController = new OrdersController(loggerStub.Object, mediatorStub.Object, orderQueriesStub.Object);
+            // Act
+            var result = await ordersController.Post(new InsertOrderCommand(0, "X"));
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task Post_Handler_Returns_False_BadRequest()
+        {
+            // Arrange
+            var mediatorStub = new Mock<IMediator>();
+            var loggerStub = new Mock<ILogger<OrdersController>>();
+            var orderQueriesStub = new Mock<IOrderQueries>();
+            mediatorStub
+                .Setup(x => x.Send(It.IsAny<IRequest<bool>>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(false));
+            var ordersController = new OrdersController(loggerStub.Object, mediatorStub.Object, orderQueriesStub.Object);
+            // Act
+            var result = await ordersController.Post(new InsertOrderCommand(0, "X"));
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_Handler_Returns_True_Ok()
+        {
+            // Arrange
+            var mediatorStub = new Mock<IMediator>();
+            var loggerStub = new Mock<ILogger<OrdersController>>();
+            var orderQueriesStub = new Mock<IOrderQueries>();
+            mediatorStub
+                .Setup(x => x.Send(It.IsAny<IRequest<bool>>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(true));
+            var ordersController = new OrdersController(loggerStub.Object, mediatorStub.Object, orderQueriesStub.Object);
+            // Act
+            var result = await ordersController.Delete("X");
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_Handler_Returns_False_BadRequest()
+        {
+            // Arrange
+            var mediatorStub = new Mock<IMediator>();
+            var loggerStub = new Mock<ILogger<OrdersController>>();
+            var orderQueriesStub = new Mock<IOrderQueries>();
+            mediatorStub
+                .Setup(x => x.Send(It.IsAny<IRequest<bool>>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(false));
+            var ordersController = new OrdersController(loggerStub.Object, mediatorStub.Object, orderQueriesStub.Object);
+            // Act
+            var result = await ordersController.Delete("X");
+            Assert.IsType<BadRequestResult>(result);
+        }
     }
 }
--- VehicleService.UnitTests/Controllers/VehiclesControllerTest.cs
+++ VehicleService.UnitTests/Controllers/VehiclesControllerTest.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using MediatR;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using VehicleService.API.Controllers;
+using VehicleService.Infrastructure.Queries.Vehicles;
+using VehicleService.API.Application.Commands.UpdateVehicleLocation;
+using Microsoft.AspNetCore.Mvc;
 
 namespace VehicleService.UnitTests.Controllers
 {
     public class VehiclesControllerTest
     {
+        [Fact]
+        public async Task UpdateVehicleLocation_Handler_Returns_True_Ok()
+        {
+            // Arrange
+            var mediatorStub = new Mock<IMediator>();
+            var loggerStub = new Mock<ILogger<VehiclesController>>();
+            var vehicleQueriesStub = new Mock<IVehicleQueries>();
+            mediatorStub
+                .Setup(x => x.Send(It.IsAny<IRequest<bool>>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(true));
+            var vehiclesController = new VehiclesController(loggerStub.Object, mediatorStub.Object, vehicleQueriesStub.Object);
+            // Act
+            var result = await vehiclesController.UpdateVehicleLocation(new UpdateVehicleLocationCommand(0, 0, 0));
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task UpdateVehicleLocation_Handler_Returns_False_BadRequest()
+        {
+            // Arrange
+            var mediatorStub = new Mock<IMediator>();
+            var loggerStub = new Mock<ILogger<VehiclesController>>();
+            var vehicleQueriesStub = new Mock<IVehicleQueries>();
+            mediatorStub
+                .Setup(x => x.Send(It.IsAny<IRequest<bool>>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(false));
+            var vehiclesController = new VehiclesController(loggerStub.Object, mediatorStub.Object, vehicleQueriesStub.Object);
+            // Act
+            var result = await vehiclesController.UpdateVehicleLocation(new UpdateVehicleLocationCommand(0, 0, 0));
+            Assert.IsType<BadRequestResult>(result);
+        }
     }
 }
