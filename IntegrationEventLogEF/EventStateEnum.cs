8de75e4d5d69a32037c95375ed52128b59c72d3a
ArtRodriguez <87017184+ArtRodriguez@users.noreply.github.com>
2022-05-15 18:18:13 +0200

    Event Bus

---
+++ IntegrationEventLogEF/EventStateEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntegrationEventLogEF
+{
+    public enum EventStateEnum
+    {
+        NotPublished = 0,
+        InProgress = 1,
+        Published = 2,
+        PublishedFailed = 3
+    }
+}
